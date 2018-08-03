b0aec2a172a3740443c3e7ccd5158b79f03e1284
Ivan_Fan <Ivan_Fan@epam.com>
2018-08-03 11:04:58 +0800

    实现了一个简单的封装， 并且实现了类似express 的路由调用方式

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const port = 3000
 const host = '127.0.0.1'
 // 实例化一个Skoa 对象
 const app = new SKoa ()
+app.get('/a', (req, res) => {
+    res.end('Page a Get')
+})
+app.post('/a', (req, res) => {
+    res.end('Page a Post')
+})
+app.del('/a', (req, res) => {
+    res.end('Page a Del')
+})
+app.get('/b', (req, res) => {
+    res.end('Page b Get')
+})
+app.post('/b', (req, res) => {
+    res.end('Page B Post')
+})
 // 调用listen 方法， 创建一个server
 app.listen(port, host, () => {
     console.log('The server is running at http://localhost:' + port)
