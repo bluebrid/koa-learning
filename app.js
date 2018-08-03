49b887985fdfae7f0ea7bac7bccf0efd4334e810
Ivan_Fan <Ivan_Fan@epam.com>
2018-08-03 15:27:05 +0800

    封装路由成一个简单的中间件

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const http = require('http')
 const SKoa = require('./lib/application')
-
+const Router = require('./lib/router')
 const port = 3000
 const host = '127.0.0.1'
 // 实例化一个Skoa 对象
 const app = new SKoa ()
-app.get('/a', (req, res) => {
+// 实例化一个Router 对象
+const router = new Router({});
+
+router.get('/a', (req, res) => {
     res.end('Page a Get')
 })
-app.post('/a', (req, res) => {
+router.post('/a', (req, res) => {
     res.end('Page a Post')
 })
-app.del('/a', (req, res) => {
+router.del('/a', (req, res) => {
     res.end('Page a Del')
 })
-app.get('/b', (req, res) => {
+router.get('/b', (req, res) => {
     res.end('Page b Get')
 })
-app.post('/b', (req, res) => {
+router.post('/b', (req, res) => {
     res.end('Page B Post')
 })
+router.all('/d',  (req, res) => {
+    res.end(`Page d ${req.method}`)
+} )
+// app 引用路由中间件
+app.use(router.routes())
 // 调用listen 方法， 创建一个server
 app.listen(port, host, () => {
     console.log('The server is running at http://localhost:' + port)
