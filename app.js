9cf3344ccfea6ee8177d48b684d5762a41b22079
Ivan_Fan <Ivan_Fan@epam.com>
2018-08-03 18:29:45 +0800

    重构中间件的功能

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 const http = require('http')
 const SKoa = require('./lib/application')
 const Router = require('./lib/router')
+const Logger = require('./lib/logger')
 const port = 3000
 const host = '127.0.0.1'
 // 实例化一个Skoa 对象
-const app = new SKoa ()
+const app = new SKoa()
 // 实例化一个Router 对象
 const router = new Router({});
 
-router.get('/a', (req, res) => {
-    res.end('Page a Get')
-})
-router.post('/a', (req, res) => {
-    res.end('Page a Post')
-})
-router.del('/a', (req, res) => {
-    res.end('Page a Del')
-})
-router.get('/b', (req, res) => {
-    res.end('Page b Get')
-})
-router.post('/b', (req, res) => {
-    res.end('Page B Post')
-})
-router.all('/d',  (req, res) => {
+router.all('/d', async (req, res) => {
+    console.log('..................')
     res.end(`Page d ${req.method}`)
-} )
+})
+app.use(new Logger({name: 'Demo'}).log())
+app.use(async (req, res, next) => {
+    console.log('===============>')
+    await next()
+})
+
 // app 引用路由中间件
 app.use(router.routes())
+app.use(async (req, res, next) => {
+    console.log('=========ffffffffffffff======>')
+    await next()
+})
+
 // 调用listen 方法， 创建一个server
 app.listen(port, host, () => {
     console.log('The server is running at http://localhost:' + port)
 })
- 
+
